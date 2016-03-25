dd6a150998c54602112fc59a0ac1a7eba4ab8ab7
William Grove <lhucomputerguy@gmail.com>
2016-03-25 15:14:49 -0400

    Added Maven compile scripts to build.gradle to get Jetty Libraries ( Not the most current version! )

--- app/src/main/java/asmagill/routeright/Fragment_PortMap.java
+++ app/src/main/java/asmagill/routeright/Fragment_PortMap.java
@@ -1,17 +1,148 @@
 package asmagill.routeright;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.os.IBinder;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.fourthline.cling.android.AndroidUpnpService;
+import org.fourthline.cling.android.AndroidUpnpServiceImpl;
+import org.fourthline.cling.android.FixedAndroidLogHandler;
+import org.fourthline.cling.model.meta.Device;
+import org.fourthline.cling.model.meta.LocalDevice;
+import org.fourthline.cling.model.meta.RemoteDevice;
+import org.fourthline.cling.registry.DefaultRegistryListener;
+import org.fourthline.cling.registry.Registry;
 
 /**
  * Created by William on 3/17/2016.
  */
-public class Fragment_PortMap extends Fragment{
+public class Fragment_PortMap extends ListFragment{
+
+
+    private ArrayAdapter<DeviceDisplay> listAdapter;
+
+    private BrowseRegistryListener registryListener = new BrowseRegistryListener();
+
+    private AndroidUpnpService upnpService;
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            upnpService = (AndroidUpnpService) service;
+
+            // Clear the list
+            listAdapter.clear();
+
+            // Get ready for future device advertisements
+            upnpService.getRegistry().addListener(registryListener);
+
+            // Now add all devices to the list we already know about
+            for (Device device : upnpService.getRegistry().getDevices()) {
+                registryListener.deviceAdded(device);
+            }
+
+            // Search asynchronously for all devices, they will respond soon
+            upnpService.getControlPoint().search();
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            upnpService = null;
+        }
+    };
+
+
     @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        org.seamless.util.logging.LoggingUtil.resetRootHandler(new FixedAndroidLogHandler());
+
+        listAdapter = new ArrayAdapter<>(this.getContext(), android.R.layout.simple_list_item_1);
+        setListAdapter(listAdapter);
+
+        // This will start the UPnP service if it wasn't already started
+        getActivity().getApplicationContext().bindService(
+                new Intent(this.getContext(), AndroidUpnpServiceImpl.class),
+                serviceConnection,
+                Context.BIND_AUTO_CREATE
+        );
+
+
+    }
+
+        @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         return inflater.inflate(R.layout.portmapping_main, container, false);
     }
+
+    protected class BrowseRegistryListener extends DefaultRegistryListener {
+
+        /* Discovery performance optimization for very slow Android devices! */
+        @Override
+        public void remoteDeviceDiscoveryStarted(Registry registry, RemoteDevice device) {
+            deviceAdded(device);
+        }
+
+        @Override
+        public void remoteDeviceDiscoveryFailed(Registry registry, final RemoteDevice device, final Exception ex) {
+
+            deviceRemoved(device);
+        }
+    /* End of optimization, you can remove the whole block if your Android handset is fast (>= 600 Mhz) */
+
+        @Override
+        public void remoteDeviceAdded(Registry registry, RemoteDevice device) {
+            deviceAdded(device);
+        }
+
+        @Override
+        public void remoteDeviceRemoved(Registry registry, RemoteDevice device) {
+            deviceRemoved(device);
+        }
+
+        @Override
+        public void localDeviceAdded(Registry registry, LocalDevice device) {
+            deviceAdded(device);
+        }
+
+        @Override
+        public void localDeviceRemoved(Registry registry, LocalDevice device) {
+            deviceRemoved(device);
+        }
+
+        public void deviceAdded(final Device device) {
+            getActivity().runOnUiThread(new Runnable() {
+                public void run() {
+                    DeviceDisplay d = new DeviceDisplay(device);
+                    int position = listAdapter.getPosition(d);
+                    if (position >= 0) {
+                        // Device already in the list, re-set new value at same position
+                        listAdapter.remove(d);
+                        listAdapter.insert(d, position);
+                    } else {
+                        listAdapter.add(d);
+                    }
+                }
+            });
+        }
+
+        public void deviceRemoved(final Device device) {
+            getActivity().runOnUiThread(new Runnable() {
+                public void run() {
+                    listAdapter.remove(new DeviceDisplay(device));
+                }
+            });
+        }
+    }
+
 }
